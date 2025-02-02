a3d4c7cfe30bd4a56d8a79e095a6fd52de91e54d
ImArjunJ <arjun.juneja4u@gmail.com>
2025-02-02 08:39:57 +0000

    Bash background compliance?

--- beaver.sh
+++ beaver.sh
@@ -1,5 +1,8 @@
 chmod +x beaver-frontend/setup-start.sh
 chmod +x beaver-backend/setup-start.sh
 
-./beaver-frontend/setup-start.sh &
-./beaver-backend/setup-start.sh &
+cd beaver-frontend
+./setup-start.sh &
+cd ..
+cd beaver-backend
+./setup-start.sh &
